5ec8b40f697dcfbb23cb0ad679f31d362673f163
Luis Carlos <luiscarlos8pe@gmail.com>
2021-11-23 17:13:33 -0300

    Criando classe Pessoa com seus atributos

---
+++ src/cadastro_pessoa/Aluno.java
@@ -0,0 +1,60 @@
+package cadastro_pessoa;
+
+public class Aluno{
+
+	
+	private String nome;
+	private int idade;
+	private String dataNascimento;
+	private String rg;
+	private String cpf;
+	private String nomeMae;
+	private String nomePai;
+	
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	public String getRg() {
+		return rg;
+	}
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getNomeMae() {
+		return nomeMae;
+	}
+	public void setNomeMae(String nomeMae) {
+		this.nomeMae = nomeMae;
+	}
+	public String getNomePai() {
+		return nomePai;
+	}
+	public void setNomePai(String nomePai) {
+		this.nomePai = nomePai;
+	}
+	
+	
+}
